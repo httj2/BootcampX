526a4ca413a0104d7c59daf55466efd7bf976020
Judith Huynh <judithuynh@gmail.com>
2020-04-30 19:27:24 +0000

    updated to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,20 +9,47 @@ const pool = new Pool({
   port: 5432
 });
 
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+
 pool
   .connect()
   .then(() => console.log("Heyyyyy I'm connected !"))
-  .then(() => console.log(process.argv))
-  .then(() => pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-  `)
-  .then(res => {
+  .then(() => pool.query(queryString, values) 
+          .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     })
   }).catch(err => console.error('query error', err.stack))
-  );
+  )
+//   // .then(() => console.log(process.argv))
+//   .then(() => pool.query(`
+//   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+//   FROM students
+//   JOIN cohorts ON cohorts.id = cohort_id
+//   WHERE cohorts.name LIKE '%${process.argv[2]}%'
+//   LIMIT ${process.argv[3] || 5};
+//   `)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     })
+//   }).catch(err => console.error('query error', err.stack))
+
+
+  
+
+
+
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,21 +9,24 @@ const pool = new Pool({
   port: 5432
 });
 
+const queryString = `SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`; 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 pool
 .connect()
 .then(() => console.log('connected!'))
-.then(() => pool.query(` SELECT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teacher;
-`) 
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack))
-);
+.then(() => pool.query(queryString, values)
+          .then(res => {
+          res.rows.forEach(user => {
+            console.log(`${user.cohort}: ${user.teacher}`);
+          })
+        }).catch(err => console.error('query error', err.stack))
+)
